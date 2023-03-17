9f6197b304f6a9a878b020787e1f2b894b801593
Parashudhara <117275535+Parashudhara@users.noreply.github.com>
2023-03-17 17:05:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const qrcode = require('qrcode-terminal');
+const { exec } = require('child_process');
+
+const { Client } = require('whatsapp-web.js');
+
+
+const client = new Client({
+  puppeteer: {
+    args: ['--no-sandbox']
+  }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+
+
+client.on('message', async message => {
+    if (message.body.startsWith('/ex')) {
+    const command = message.body.slice(4);
+    const result = await runCommand(command);
+    message.reply(result);
+  }
+});
+
+function runCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                reject(error);
+            } else {
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+                resolve(stdout);
+            }
+        });
+    });
+}
+
+client.initialize();
