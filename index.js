205fea627b1ccce6deb7c9bc1ca3dc63cf1dedac
Parashudhara <117275535+Parashudhara@users.noreply.github.com>
2023-03-17 17:46:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const qrcode = require('qrcode-terminal');
 const { exec } = require('child_process');
+const express = require('express');
 
 const { Client } = require('whatsapp-web.js');
 
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 const client = new Client({
   puppeteer: {
@@ -11,38 +16,36 @@ const client = new Client({
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+  qrcode.generate(qr, {small: true});
 });
-
-
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+  console.log('Client is ready!');
 });
 
-
-
-client.on('message', async message => {
-    if (message.body.startsWith('/ex')) {
-    const command = message.body.slice(4);
-    const result = await runCommand(command);
-    message.reply(result);
-  }
+app.post('/api/run-command', async (req, res) => {
+  const command = req.body.command;
+  const result = await runCommand(command);
+  res.send(result);
 });
 
 function runCommand(command) {
-    return new Promise((resolve, reject) => {
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                reject(error);
-            } else {
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
-                resolve(stdout);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+      } else {
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        resolve(stdout);
+      }
     });
+  });
 }
 
 client.initialize();
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
